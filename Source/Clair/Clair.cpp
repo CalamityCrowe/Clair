c5057f65821b67cfc45c85810717872051b4da0c
Kieran T <kieran123416@gmail.com>
2025-09-05 13:42:30 +0100

    Initial commit

---
+++ Source/Clair/Clair.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Clair.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Clair, "Clair" );
